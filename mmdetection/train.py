5f9fbe82257b4286e25a8ec5db81d014fdef9930
HyeonKi Kim <hyeonrl98@gmail.com>
2024-10-14 23:08:41 +0900

    Separate config from main

--- mmdetection/train.py
+++ mmdetection/train.py
@@ -1,70 +1,39 @@
-from mmcv import Config
+import sys
+import argparse
 from mmdet.datasets import build_dataset
 from mmdet.models import build_detector
 from mmdet.apis import train_detector
-from mmdet.datasets import (build_dataloader, build_dataset,
-                            replace_ImageToTensor)
-from mmdet.utils import get_device
 
-
-model_dir = 'cascade_rcnn'
-model_name = 'cascade_rcnn_r101_fpn_1x_coco'
-work_dir = f'./work_dirs/{model_name}'
-
-def main() :
-    classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
-           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
-
-    cfg = Config.fromfile(f'./configs/{model_dir}/{model_name}.py')
-
-    root='/data/ephemeral/home/dataset/'
-
-    cfg.data.train.classes = classes
-    cfg.data.train.img_prefix = root
-    cfg.data.train.ann_file = root + 'train_split.json' # train json 정보
-
-    cfg.data.val.classes = classes
-    cfg.data.val.img_prefix = root
-    cfg.data.val.ann_file = root + 'val_split.json' # train json 정보
-
-    cfg.data.test.classes = classes
-    cfg.data.test.img_prefix = root
-    cfg.data.test.ann_file = root + 'test.json' # test json 정보
-
-    cfg.data.samples_per_gpu = 8
-
-    cfg.seed = 2024
-    cfg.gpu_ids = [0]
-    cfg.work_dir = work_dir
-
-    cfg.evaluation = dict(
-        interval=1,
-        metric='bbox',
-        save_best='auto',
-        by_epoch=True
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train object detection model")
+    parser.add_argument(
+        "--config", 
+        default="config_default",  # 기본 설정 파일 이름
+        help="Configuration file to use, e.g., config_cascade_rcnn_r101"
     )
+    return parser.parse_args()
 
-    for bbox_head in cfg.model.roi_head.bbox_head:
-        bbox_head.num_classes = 10
+def main():
+    args = parse_args()
 
-    cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
-    cfg.checkpoint_config = dict(max_keep_ckpts=3, interval=1)
+    # 설정 파일의 경로를 동적으로 설정
+    config_module = f'configs._configs_.{args.config}'
+    try:
+        # 동적 임포트: 해당 경로에서 get_cfg 함수를 가져온다
+        config = __import__(config_module, fromlist=['get_cfg'])
+        cfg = config.get_cfg()
+    except ImportError as e:
+        print(f"Failed to import config: {config_module}")
+        raise e
 
-    cfg.log_config = dict(
-    interval=50,
-    hooks=[
-        dict(type='TextLoggerHook'),
-        dict(type='TensorboardLoggerHook'),
-    ])
-
-    cfg.device = get_device()
-
+    # 데이터셋 및 모델 생성
     datasets = [build_dataset(cfg.data.train)]
-
     model = build_detector(cfg.model)
     model.init_weights()
 
+    # 모델 학습
     train_detector(model, datasets, cfg, distributed=False, validate=True)
 
 if __name__ == '__main__':
+    sys.path.append('./')
     main()
