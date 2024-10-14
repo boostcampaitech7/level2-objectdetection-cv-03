ccd3fa4f72b460585347788b73a718fd9bac183c
Yoon <hsy0320@hufs.ac.kr>
2024-10-14 19:38:38 +0900

    custom_faster_rcnn upload

---
+++ mmdetection_base/configs/faster_rcnn/custom_faster_rcnn.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../_base_/models/custom_faster_rcnn_fpn.py',
+    '../_base_/datasets/custom_coco_detection.py',
+    '../_base_/schedules/custom_schedule.py',
+    '../_base_/default_runtime.py'
+]
