191a3df1bea484a7614d366ac0f78c9601cf39f0
HyeonKi Kim <hyeonrl98@gmail.com>
2024-10-16 10:40:35 +0900

    Add cascade rcnn swin

---
+++ mmdetection/configs/_configs_/cascade_rcnn_swin.py
@@ -0,0 +1,60 @@
+# config.py
+from mmcv import Config
+from mmdet.utils import get_device
+
+model_dir = 'cascade_rcnn'
+model_name = 'cascade_rcnn_swin_fpn_1x_coco'
+work_dir = f'/data/ephemeral/home/work_dirs/{model_name}'
+
+def get_cfg():
+    classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+               "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+
+    cfg = Config.fromfile(f'./configs/{model_dir}/{model_name}.py')
+
+    root = '/data/ephemeral/home/dataset/'
+
+    cfg.data.train.classes = classes
+    cfg.data.train.img_prefix = root
+    cfg.data.train.ann_file = root + 'train_split.json'  # train json 정보
+
+    cfg.data.val.classes = classes
+    cfg.data.val.img_prefix = root
+    cfg.data.val.ann_file = root + 'val_split.json'  # val json 정보
+
+    cfg.data.test.classes = classes
+    cfg.data.test.img_prefix = root
+    cfg.data.test.ann_file = root + 'test.json'  # test json 정보
+
+    cfg.data.samples_per_gpu = 6
+
+    cfg.seed = 2024
+    cfg.gpu_ids = [0]
+    cfg.work_dir = work_dir
+
+    cfg.evaluation = dict(
+        interval=1,
+        metric='bbox',
+        save_best='auto',
+        by_epoch=True
+    )
+
+    for bbox_head in cfg.model.roi_head.bbox_head:
+        bbox_head.num_classes = 10
+
+    cfg.runner.max_epochs = 20
+
+    cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+    cfg.checkpoint_config = dict(max_keep_ckpts=3, interval=1)
+
+    cfg.log_config = dict(
+        interval=50,
+        hooks=[
+            dict(type='TextLoggerHook'),
+            dict(type='TensorboardLoggerHook'),
+        ]
+    )
+
+    cfg.device = get_device()
+
+    return cfg
---
+++ mmdetection/configs/cascade_rcnn/cascade_rcnn_swin_fpn_1x_coco.py
@@ -0,0 +1,30 @@
+_base_ = [
+    '../_base_/models/cascade_rcnn_r50_fpn.py',
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window12_384_22kto1k.pth'  # noqa
+
+
+model = dict(
+    backbone=dict(
+        _delete_=True,
+        type='SwinTransformer',
+        embed_dims=128,
+        depths=[2, 2, 18, 2],
+        num_heads=[4, 8, 16, 32],
+        window_size=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        with_cp=False,
+        convert_weights=True,
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)
+    ),
+    neck=dict(in_channels=[128, 256, 512, 1024]))
